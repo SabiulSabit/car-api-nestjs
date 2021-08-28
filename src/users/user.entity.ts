2deb3631ef1ebb3568be6e9ba5cfb14667278991
SabitTheBot <iamsabit99@gmail.com>
2021-08-28 22:15:17 +0600

    add some hooks in the user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class User {
@@ -10,4 +10,19 @@ export class User {
 
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log("Inserted User with ID: ", this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log("Updated User with ID: ", this.id);
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log("Remove User with ID: ", this.id);
+    }
 }
