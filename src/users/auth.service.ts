20bcbd0e601a43a393aa56bc67c238b8b7d4c047
SabitTheBot <iamsabit99@gmail.com>
2021-09-08 21:51:15 +0600

    password hash done

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util'
 import { UsersService } from './users.service'
+
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthService {
@@ -9,10 +13,18 @@ export class AuthService {
     }
 
     async singup(email: string, password: string) {
+
+        //check if the email is in use
         let users = await this.userService.find(email);
         if (users.length) {
             throw new BadRequestException('Email in Use')
         }
+
+        //hash the password
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        const result = salt + '.' + hash.toString('hex');
+
     }
 
     signin() {
