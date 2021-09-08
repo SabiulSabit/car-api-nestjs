9295af2ba75c6a72ef89c2bf74e4c1c48ce5938a
SabitTheBot <iamsabit99@gmail.com>
2021-09-08 21:33:44 +0600

    user email check done for singup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service'
 
 @Injectable()
 export class AuthService {
 
     constructor(private userService: UsersService) {
+
+    }
+
+    async singup(email: string, password: string) {
+        let users = await this.userService.find(email);
+        if (users.length) {
+            throw new BadRequestException('Email in Use')
+        }
+    }
+
+    signin() {
 
     }
 }
