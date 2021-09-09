3d9699b22f48b5df0387711339ba93452ce8f3a3
SabitTheBot <iamsabit99@gmail.com>
2021-09-09 22:38:58 +0600

    add singin method in auth service

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util'
 import { UsersService } from './users.service'
@@ -31,7 +31,24 @@ export class AuthService {
         return user;
     }
 
-    signin() {
+    async signin(email: string, password: string) {
 
+        //find user with email
+        const [user] = await this.userService.find(email)
+
+        //if user not found
+        if (!user) {
+            throw new NotFoundException("User Not Found")
+        }
+
+        //check password
+        const [salt, storedHash] = user.password.split('.');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash === hash.toString('hex')) {
+            return user;
+        } else {
+            throw new BadRequestException("Password Don't Match")
+        }
     }
 }
