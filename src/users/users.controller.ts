63890d17139c895f8a013a6d366c748a20e31149
SabitTheBot <iamsabit99@gmail.com>
2021-09-12 21:28:05 +0600

    use basic custome decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service'
 import { Serialize } from '../interceptors/serialize.interceptors'
 import { UserDto } from './dtos/user.dto'
 import { AuthService } from './auth.service'
+import { CurrentUser } from './decorators/current-user.decorator'
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -14,9 +15,14 @@ export class UsersController {
     constructor(private usersService: UsersService, private authService: AuthService) {
     }
 
+    // @Get('/currentuser')
+    // currentUser(@Session() session: any) {
+    //     return this.usersService.findOne(session.userId);
+    // }
+
     @Get('/currentuser')
-    currentUser(@Session() session: any) {
-        return this.usersService.findOne(session.userId);
+    currentUser(@CurrentUser() user: string) {
+        return user;
     }
 
     @Post('/signout')
