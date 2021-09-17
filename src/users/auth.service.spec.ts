3893ff01dadb57c0ca8c269dabdac61e822175eb
SabitTheBot <iamsabit99@gmail.com>
2021-09-17 22:55:33 +0600

    add descrive in testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,22 +2,28 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
+describe('AuthService', () => {
+    let service: AuthService;
 
-it("can", async () => {
+    beforeEach(async () => {
+        // fake user service
+        const fakeUsersService = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password })
+        }
+        const module = await Test.createTestingModule({
+            providers: [AuthService, {
+                provide: UsersService,
+                useValue: fakeUsersService
+            }]
+        }).compile();
 
-    // fake user service
-    const fakeUsersService = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password })
-    }
-    const module = await Test.createTestingModule({
-        providers: [AuthService, {
-            provide: UsersService,
-            useValue: fakeUsersService
-        }]
-    }).compile();
+        service = module.get(AuthService);
+    })
 
-    const service = module.get(AuthService);
 
-    expect(service).toBeDefined();
+    it("can", async () => {
+        expect(service).toBeDefined();
+    })
 })
+
