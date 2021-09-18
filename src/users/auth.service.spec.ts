416b243159251acad1c06d360f4bd0cbb7d25fb2
SabitTheBot <iamsabit99@gmail.com>
2021-09-18 15:06:05 +0600

    add existing user show error testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,15 +1,17 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 describe('AuthService', () => {
     let service: AuthService;
+    let fakeUsersService: Partial<UsersService>;
 
     beforeEach(async () => {
         // fake user service
-        const fakeUsersService = {
+        fakeUsersService = {
             find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password })
+            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
         }
         const module = await Test.createTestingModule({
             providers: [AuthService, {
@@ -34,6 +36,17 @@ describe('AuthService', () => {
         const [salt, hash] = user.password.split('.');
         expect(salt).toBeDefined();
         expect(hash).toBeDefined();
+    })
+
+    it('throws an error if user sign up with an exsisting email', async () => {
+        fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: 'ads' } as User])
+
+        try {
+            await service.singup('test@test.com', 'asdf')
+        } catch (err) {
+            // done();
+        }
+
     })
 })
 
