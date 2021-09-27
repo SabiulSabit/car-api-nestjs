4f6f0c500762c44a1374cc8634a73f5bf08aacbb
SabitTheBot <iamsabit99@gmail.com>
2021-09-27 22:27:28 +0600

    use apprve reaport dto in controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -6,7 +6,7 @@ import { AuthGuard } from '../guards/auth.guard'
 import { CurrentUser } from '../users/decorators/current-user.decorator'
 import { User } from '../users/user.entity'
 import { Serialize } from '../interceptors/serialize.interceptors';
-
+import { ApproveReportDto } from './dtos/approve-report.dto'
 
 
 
@@ -22,5 +22,10 @@ export class ReportsController {
     @Serialize(ReportDto)
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
         return this.reportService.create(body, user);
+    }
+
+    @Patch('/:id')
+    approvedReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+
     }
 }
